17995480e961b5a201e4dc5876b5bfdffa08b484
Konnor Beaulier <konbeaulier@gmail.com>
2021-08-27 10:40:08 -0700

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Konnor Beaulier on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
